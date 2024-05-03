7c1635014da06061a503f1aa11db328712160b57
Lucas Gelenski <gelenskilucas@gmail.com>
2024-05-03 11:57:41 -0300

    Refatorando função de alterar_estado_restaurante

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def opcao_invalida():
 def exibir_opcoes():
     print("1 - Cadastrar restaurante")
     print("2 - Listar restaurante")
-    print("3 - Ativar restaurante")
+    print("3 - Ativar/Desativar restaurante")
     print("4 - Sair\n")
 
 
@@ -79,8 +79,8 @@ def alterar_estado_restaurantes():
             mensagem = f"O restaurante {nome_restaurante} foi ativado com sucesso! " if restaurante["ativo"] else f"O restaurante {
                 nome_restaurante} foi desativado com sucesso!"
             print(mensagem)
-        if not restaurante_encontrado:
-            print(f"O restaurante não foi encontrado.")
+    if not restaurante_encontrado:
+        print(f"O restaurante não foi encontrado.")
 
     voltar_menu_principal()
 
